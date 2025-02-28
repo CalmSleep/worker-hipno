9e4dbadb6f5fae025174540407c1f4745b6d265d
nicolasboland <nicolasboland@calmessimple.com.ar>
2025-02-28 12:28:10 -0300

    redirect hipno

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+export default {
+	async fetch(request: Request, env: any, ctx: ExecutionContext): Promise<Response> {
+	  const url = new URL(request.url);
+	  const path = url.pathname;
+	  const searchParams = url.searchParams;
+  
+	  // Rutas que deben ir al servidor (165.140.86.130)
+	  const serverPaths = ["/public_html", "/lab", "/wp-admin", "/headless"];
+	  const isServerRoute = serverPaths.some(route => path.startsWith(route));
+  
+	  // Ruta específica para /gandalf-api/token/index.php?param=*
+	  console.log(path);
+	  
+	  const isGandalfRoute = path === "/gandalf-api/token/index.php"/*  && searchParams.has("param"); */
+
+	  const isGandalftest = path === "/gandalf-api/health-check"
+  
+	  if (isServerRoute) {
+		// Redirige al servidor (165.140.86.130)
+		const serverUrl = `http://165.140.86.130${path}${url.search}`;
+		
+		// Clona la request y modifica el "Host" si es necesario
+		const newRequest = new Request(serverUrl, {
+		  method: request.method,
+		  headers: request.headers,
+		  body: request.body
+		});
+  
+		return fetch(newRequest);
+	  } else if (isGandalfRoute) {
+		// Redirige a https://gandalf-project-middleware.onrender.com/gandalf-api/token
+		const gandalfUrl = `https://gandalf-project-middleware.onrender.com/gandalf-api/token${url.search}`;
+  
+		// Clona la request y mantiene las cookies
+		const newRequest = new Request(gandalfUrl, {
+		  method: request.method,
+		  headers: request.headers,
+		  body: request.body
+		});
+  
+		// Envía la solicitud y devuelve la respuesta
+		return fetch(newRequest);
+	  }
+
+	  if (isGandalftest) {
+		// Redirige a https://gandalf-project-middleware.onrender.com/gandalf-api/token
+		const gandalfUrl = `https://gandalf-project-middleware.onrender.com`;
+  
+		// Clona la request y mantiene las cookies
+		const newRequest = new Request(gandalfUrl, {
+		  method: request.method,
+		  headers: request.headers,
+		  body: request.body
+		});
+  
+		// Envía la solicitud y devuelve la respuesta
+		return fetch(newRequest);
+	  }
+  
+	  // Resto del tráfico al frontend (configurado en DNS de Cloudflare)
+	  return fetch(request);
+	}
+  } satisfies ExportedHandler;
