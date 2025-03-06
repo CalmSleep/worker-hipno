a6b9e309776d5eac63d7b35f68a04d2d65c7dba3
nicolasboland <nicolasboland@calmessimple.com.ar>
2025-03-06 20:19:14 -0300

    updtes

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,127 @@ export default {
 	  const url = new URL(request.url);
 	  const path = url.pathname;
 	  const searchParams = url.searchParams;
-  
-	  // Rutas que deben ir al servidor (165.140.86.130)
-	  const serverPaths = ["/public_html", "/lab", "/wp-admin", "/headless"];
-	  const isServerRoute = serverPaths.some(route => path.startsWith(route));
-  
-	  // Ruta específica para /gandalf-api/token/index.php?param=*
-	  console.log(path);
-	  
-	  const isGandalfRoute = path === "/gandalf-api/token/index.php"/*  && searchParams.has("param"); */
 
-	 /*  const isGandalftest = path === "/gandalf-api/health-check" */
+	  // Redirecciones específicas para Gandalf
+	  if (path.startsWith("/gandalf-api")) {
+		const gandalfUrl = `https://dev-gandalf-ar.onrender.com${path}${url.search}`;
+		const newRequest = new Request(gandalfUrl, {
+		  method: request.method,
+		  headers: {
+			...Object.fromEntries(request.headers),
+			"Host": "hipno.com.ar"
+		  },
+		  body: request.body
+		});
+		return fetch(newRequest);
+	  }
+
+	  if (path.startsWith("/prod-gandalf-api")) {
+		const gandalfUrl = `https://gandalf-api-prod.onrender.com${path}${url.search}`;
+		const newRequest = new Request(gandalfUrl, {
+		  method: request.method,
+		  headers: {
+			...Object.fromEntries(request.headers),
+			"Host": "hipno.com.ar"
+		  },
+		  body: request.body
+		});
+		return fetch(newRequest);
+	  }
+
+	  if (path.startsWith("/gandalf-v2-prod")) {
+		const gandalfUrl = `https://gandalf-v2-arg-prod.onrender.com${path}${url.search}`;
+		const newRequest = new Request(gandalfUrl, {
+		  method: request.method,
+		  headers: {
+			...Object.fromEntries(request.headers),
+			"Host": "hipno.com.ar"
+		  },
+		  body: request.body
+		});
+		return fetch(newRequest);
+	  }
+
+	  if (path.startsWith("/gandalf-v2-dev")) {
+		const gandalfUrl = `https://gandalf-v2-arg-dev.onrender.com${path}${url.search}`;
+		const newRequest = new Request(gandalfUrl, {
+		  method: request.method,
+		  headers: {
+			...Object.fromEntries(request.headers),
+			"Host": "hipno.com.ar"
+		  },
+		  body: request.body
+		});
+		return fetch(newRequest);
+	  }
+
+	  const rutas = [
+		"/old-site/",
+		"/cancelar-compra",
+		"/api/revalidate",
+		"/api",
+		"/_next",
+		"/compromiso-descansados",
+		"/locales-calm",
+		"/opiniones-reales-de-clientes-reales",
+		"/preguntas-frecuentes",
+		"/quienes-somos",
+		"/terminos-y-condiciones",
+		"/colchones",
+		"/bases",
+		"/almohadas",
+		"/ropa-de-cama",
+		"/muebles",
+		"/producto",
+		"/favicon",
+		"/cyber-monday",
+		"/mantenimiento",
+		"/gandalf-api",
+		"/prod-gandalf-api",
+		"/gandalf-v2-prod",
+		"/gandalf-v2-dev",
+		"/siestario-calm",
+		"/test-pago",
+		"/404",
+		"/pick-up",
+		"/envios",
+		"/30-noches-de-prueba-en-casa",
+		"/feria",
+		"/checkout-pago",
+		"/metodos-de-pago",
+		"/hot-sale",
+		"/accesorios",
+		"/sitemap.xml",
+		"/rueda-de-la-calma",
+		"/seguimiento",
+		"/clear-cache",
+		"/test-nico",
+		"/fonts",
+		"/.well-known"
+	  ];
+	  const isFrontRoute = rutas.some(route => path.startsWith(route)) || path === "/";
   
-	  if (isServerRoute) {
-		// Redirige al servidor (165.140.86.130)
-		//va a tener que ser a subdominio el server porque cloudflare no pueed mandar solictudes a ips directas con proxy activado, sin proxy no funca el worker
-		//termino siendo al reves en hipno porque el server con subdominio tiraba error decertificado, tuve que hacer el reves porque vercel le da su certificado
-		const serverUrl = `http://hipno.com.ar${path}${url.search}`;
+	  if (path === "/") {
+		const shouldGoToServer = searchParams.has("wc-ajax") || 
+								searchParams.has("elementor-preview") || 
+								searchParams.has("ver");
 		
-		// Clona la request y modifica el "Host" si es necesario
+		if (shouldGoToServer) {
+		  const serverUrl = `https://test.hipno.com.ar${path}${url.search}`;
+		  const newRequest = new Request(serverUrl, {
+			method: request.method,
+			headers: {
+			  ...Object.fromEntries(request.headers),
+			  "Host": "hipno.com.ar"
+			},
+			body: request.body
+		  });
+		  return fetch(newRequest);
+		}
+	  }
+  
+	  if (isFrontRoute) {
+		const serverUrl = `https://test.hipno.com.ar${path}${url.search}`;
 		const newRequest = new Request(serverUrl, {
 		  method: request.method,
 		  headers: {
@@ -32,43 +134,8 @@ export default {
 		});
   
 		return fetch(newRequest);
-	  } else if (isGandalfRoute) {
-		// Redirige a https://gandalf-project-middleware.onrender.com/gandalf-api/token
-		const gandalfUrl = `https://gandalf-project-middleware.onrender.com/gandalf-api/token${url.search}`;
-  
-		// Clona la request y mantiene las cookies
-		const newRequest = new Request(gandalfUrl, {
-		  method: request.method,
-		  headers: {
-			...Object.fromEntries(request.headers),
-			"Host": "hipno.com.ar"
-		  },
-		  body: request.body
-		});
-  
-		// Envía la solicitud y devuelve la respuesta
-		return fetch(newRequest);
 	  }
-/* 
-	  if (isGandalftest) {
-		// Redirige a https://gandalf-project-middleware.onrender.com/gandalf-api/token
-		const gandalfUrl = `https://gandalf-project-middleware.onrender.com`;
-  
-		// Clona la request y mantiene las cookies
-		const newRequest = new Request(gandalfUrl, {
-		  method: request.method,
-		  headers: {
-			...Object.fromEntries(request.headers),
-			"Host": "hipno.com.ar"
-		  },
-		  body: request.body
-		});
-  
-		// Envía la solicitud y devuelve la respuesta
-		return fetch(newRequest);
-	  } */
-  
-	  // Resto del tráfico al frontend (configurado en DNS de Cloudflare)
+
 	  return fetch(request);
 	}
   } satisfies ExportedHandler;
